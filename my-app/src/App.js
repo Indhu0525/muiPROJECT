7af05b59c47d7f7cb45887de01c740f128b001dc
Indhu0525 <indu01401@hmail.com>
2024-07-16 11:45:07 +0530

    installing

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import Button from '@mui/material/Button';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <Button variant="contained" color="primary">
+        Hello
+      </Button>
+    </div>
+  );
+}
+
+export default App;
